a606265f38093cd121c24ee064808f7e7b3dd9a9
JulioFertry <juliofertry@gmail.com>
2025-02-14 21:05:13 +0100

    Inicio

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ADA05_API"
---
+++ src/main/kotlin/com/es/mongoapi/ADA05_API/Ada05ApiApplication.kt
@@ -0,0 +1,11 @@
+package com.es.mongoapi.ADA05_API
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ada05ApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<Ada05ApiApplication>(*args)
+}
